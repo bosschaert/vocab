6156f916ddb496e0b5f90c90d32a1bba9e3dc8c3
David Bosschaert <david.bosschaert@gmail.com>
2024-02-05 17:32:36 +0000

    Basic controls

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,7 @@
+function checkWord() {
+    console.log("checking");
+    const input = window.document.getElementById("guess");
+    console.log("Text: " + input.value)
+}
+
 console.log("*** Hello There");
